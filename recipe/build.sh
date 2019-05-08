1b303904d446714c686d2adf3fb348800a5c2e34
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-05-08 15:58:07 +0100

    Add missing shared objects and headers

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,3 +3,25 @@ export DMLC_CORE_PATH=$PREFIX
 make -j$CPU_COUNT
 
 cp lib/* $PREFIX/lib/.
+
+mkdir $PREFIX/lib/arithmetic
+mkdir $PREFIX/include/arithmetic
+cp build/arithmetic/* $PREFIX/lib/arithmetic/.
+cp src/arithmetic/*.h $PREFIX/include/arithmetic/.
+
+mkdir $PREFIX/lib/ir
+mkdir $PREFIX/include/ir
+cp build/ir/* $PREFIX/lib/ir/.
+cp src/ir/*.h $PREFIX/include/ir/.
+
+mkdir $PREFIX/lib/base
+mkdir $PREFIX/include/base
+cp build/base/* $PREFIX/lib/base/.
+cp src/base/*.h $PREFIX/include/base/.
+
+mkdir $PREFIX/lib/tvm
+mkdir $PREFIX/lib/tvm/node
+mkdir $PREFIX/include/tvm
+mkdir $PREFIX/include/tvm/node
+cp build/tvm/node/* $PREFIX/lib/tvm/node/.
+cp src/tvm/node/*.h $PREFIX/include/tvm/node/.
